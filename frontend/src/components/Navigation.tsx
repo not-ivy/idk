2fe8db46c63aac23210db9b09c9c0fe2edcb83ba
i-spin <me@i-sp.in>
2022-07-29 14:52:38 -0700

    add source in navi

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -5,7 +5,7 @@ export default function Navigation() {
       <span>&nbsp;-&nbsp;</span>
       <a href="#">Graph</a>
       <span>&nbsp;-&nbsp;</span>
-      <a href="#">Settings</a>
+      <a href="https://github.com/i-spin/idk" target="_blank">Source</a>
     </div>
   )
 }
