d49b2d88653e348889bdfdaab227d078b5158c02
i-spin <me@i-sp.in>
2022-07-29 11:34:33 -0700

    vite bind to port env var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
+import 'dotenv/config';
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  server: {
+    port: process.env.PORT || 3000,
+  }
 })
