66f1cad77664efd27dfcef6f6d140c3d58bc9309
i-spin <me@i-sp.in>
2022-07-29 10:12:12 -0700

    add home page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import 'dotenv/config'
 import cero from '0http'
-import Mood from './routes/mood.js'
+import Mood from './routes/auth/mood.js'
 import Authorization from './routes/authorization.js'
+import Home from './routes/Home.js'
 const { router, server } = cero()
 
 const port = process.env.PORT || 8080
 
-router.use('/post', Authorization)
-router.post('/post/mood', Mood)
+router.get('/', Home)
+
+router.use('/auth', Authorization)
+router.post('/auth/mood', Mood)
 
 server.listen(port)
 
---
+++ backend/src/routes/home.js
@@ -0,0 +1,6 @@
+export default function Home(req, res) {
+  res.end(`Hello :D
+This is the backend for my project
+Source code: https://github.com/i-spin/idk
+`)
+}
