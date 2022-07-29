2f2f4094ed625d207be4b837d836ec09117f8b2d
i-spin <me@i-sp.in>
2022-07-29 14:15:10 -0700

    bigint trolling

--- backend/src/routes/mood.js
+++ backend/src/routes/mood.js
@@ -2,7 +2,11 @@ import { getLastScore } from '../util/prisma.js';
 
 export default function Mood(req, res) {
   getLastScore().then((data) => {
-    res.end(JSON.stringify(data));
+    res.end(JSON.stringify(data, (_key, value) => (
+      typeof value === 'bigint'
+        ? parseInt(value.toString(), 10)
+        : value
+    )));
   }).catch((e) => {
     res.end(e.toString());
   });
