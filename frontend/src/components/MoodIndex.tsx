15f3b1c0cb0c11899332e77a2c533492bfd0aaed
i-spin <me@i-sp.in>
2022-07-29 12:17:45 -0700

    get mood index from backend

--- frontend/src/components/MoodIndex.tsx
+++ frontend/src/components/MoodIndex.tsx
@@ -1,5 +1,12 @@
+import useSWR from "swr";
+
 export default function MoodIndex() {
+  const { data, error } = useSWR('https://api.idk.i-sp.in/mood')
+
+  if (error) return <p>Failed to load<br /><pre>{error.toString()}</pre></p>
+  if (!data) return <p>Loading</p>
+
   return (
-    <p className="dark:text-white">Current mood index:<br /><span>{}</span></p>
+    <p>Mood Index: <span>{data.score}</span><br />Updated:<span>{new Date(data.date* 1000).toLocaleString()}</span></p>
   )
 }
