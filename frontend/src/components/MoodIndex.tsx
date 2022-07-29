9325eb9677091a604a7d66bab4a84c162028c0bd
i-spin <me@i-sp.in>
2022-07-29 13:50:04 -0700

    mood index done

--- frontend/src/components/MoodIndex.tsx
+++ frontend/src/components/MoodIndex.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from 'preact/hooks';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
 export default function MoodIndex() {
   const [moodData, setMoodData] = useState<{ id: number, date: number, score: number } | undefined>(undefined);
 
   useEffect(() => {
     fetch('https://api.idk.i-sp.in/mood')
       .then((res) => res.json())
-      .then((data) => setMoodData(data))
+      .then((data) => { setMoodData(data); console.log(data)})
   }, [])
 
   return (
     <div>
-      Mood Index: <br />
       {!moodData ? <span>Loading...</span> :
       <div className='text-center'>
-        <span>{moodData.score}</span> <br />
-        Last Updated: <br />
-        <span>{new Date(moodData.date).toLocaleDateString()}</span>
+        <span>Mood Index: {moodData.score}</span> <br />
+        <span>Updated: {dayjs.unix(moodData.date / 1000).fromNow()}</span>
       </div>}
     </div>
   )
