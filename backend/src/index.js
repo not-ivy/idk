a431b6a5a0d81ceaed2d4817a6eeb0fe26846ee3
i-spin <me@i-sp.in>
2022-07-30 12:46:55 -0700

    add swr header on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import Authorization from './routes/post/authorization.js';
 import Home from './routes/home.js';
 import Data from './routes/post/data.js';
 import Mood from './routes/mood.js';
-import CORS from './routes/cors.js';
+import Middleware from './routes/Middleware.js';
 import Quote from './routes/quote.js';
 import Beat from './routes/beat.js';
 import Presence from './routes/presence.js';
@@ -15,7 +15,7 @@ const { router, server } = cero();
 
 const port = process.env.PORT || 8080;
 
-router.use('/', CORS);
+router.use('/', Middleware);
 router.get('/', Home);
 router.get('/mood', Mood);
 router.get('/quote', Quote);
--- backend/src/routes/Middleware.js
+++ backend/src/routes/Middleware.js
@@ -1,4 +1,5 @@
-export default function CORS(req, res, next) {
+export default function Middleware(req, res, next) {
+  res.setHeader('Cache-Control', 'max-age=1, stale-while-revalidate=10');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
   res.setHeader('Access-Control-Max-Age', 2592000);
