cc7eca9b8048f93e4263ba7a3895de18bf3baa08
i-spin <me@i-sp.in>
2022-07-29 22:37:51 -0700

    fix crash and center custom status

--- frontend/src/components/DiscordStatus.tsx
+++ frontend/src/components/DiscordStatus.tsx
@@ -10,13 +10,14 @@ export default function DiscordStatus() {
   }, [])
 
   return (
-    <p className="dark:text-white">
+    <p className="dark:text-white text-center">
       Current discord status: {!presence ? "loading..." : presence.status}<br />
       {!presence ? "loading..." :
-        <div>
-          <img src={presence.custom.emoji} className="w-4 h-4" alt="emoji" />
-          {presence.custom.name} {presence.custom.text}
-        </div>
+        !presence.custom ? "" :
+          <div className='flex justify-center'>
+            {presence.custom.emoji ? <img src={presence.custom.emoji} className="w-5 h-5 mr-4" alt="emoji" /> : <></>}
+            {presence.custom.name} {presence.custom.text ? `- ${presence.custom.text}` : ''}
+          </div>
       }
     </p>
   )
