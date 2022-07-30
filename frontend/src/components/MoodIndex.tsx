59c016c3027728c695b1157c2ece26283c3ee581
i-spin <me@i-sp.in>
2022-07-29 21:09:50 -0700

    change how loading works for moodindex

--- frontend/src/components/MoodIndex.tsx
+++ frontend/src/components/MoodIndex.tsx
@@ -10,16 +10,13 @@ export default function MoodIndex() {
   useEffect(() => {
     fetch('https://api.idk.i-sp.in/mood')
       .then((res) => res.json())
-      .then((data) => { setMoodData(data); console.log(data)})
+      .then((data) => { setMoodData(data); console.log(data) })
   }, [])
 
   return (
-    <div>
-      {!moodData ? <span>Loading...</span> :
-      <div className='text-center'>
-        <span>Mood Index: {moodData.score}</span> <br />
-        <span>Updated {dayjs.unix(moodData.date / 1000).fromNow()}</span>
-      </div>}
+    <div className='text-center'>
+      <p>Mood Index: {!moodData ? <span>0</span> : <span>{moodData.score}</span>}</p>
+      <p>Updated {!moodData ? <span>loading...</span> : <span>{dayjs.unix(moodData.date / 1000).fromNow()}</span>}</p>
     </div>
   )
 }
