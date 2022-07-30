8dcf36bbd652e99c1f8c0f644822fe904619a9d7
i-spin <me@i-sp.in>
2022-07-29 21:05:56 -0700

    update last seen component

--- frontend/src/components/LastSeen.tsx
+++ frontend/src/components/LastSeen.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'preact/hooks';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
 export default function LastSeen() {
-    return (
-      <p className="dark:text-white">Last seen:<br />{}</p>
-    )
+  const [heartbeat, setHeartbeat] = useState<{ id: number, device: string, time: number } | undefined>(undefined);
+
+  useEffect(() => {
+    fetch('https://api.idk.i-sp.in/beat')
+      .then((res) => res.json())
+      .then((data) => { setHeartbeat(data); console.log(data) })
+  }, [])
+  return (
+    <p className='text-center'>
+      Last seen:<br />
+      {!heartbeat ? <span>loading...</span> : <span>{dayjs.unix(heartbeat.time / 1000).fromNow()}</span>}
+    </p>
+  )
 }
