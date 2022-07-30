58c471f6f473ff1005b447257312c233186b0084
i-spin <me@i-sp.in>
2022-07-30 14:09:24 -0700

    update frontend

--- frontend/src/components/DiscordStatus.tsx
+++ frontend/src/components/DiscordStatus.tsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'preact/hooks';
 
+type Activity = {
+  name: string | null,
+  emoji: string | null,
+  state: string | null
+}
+
+type DiscordState = {
+  status: string,
+  activities: Activity[] | undefined
+}
+
 export default function DiscordStatus() {
-  const [presence, setPresence] = useState<{ status: string, custom: { name: string, text: string, emoji: string } | undefined } | undefined>(undefined);
+  const [presence, setPresence] = useState<DiscordState | undefined>(undefined);
 
   useEffect(() => {
     fetch('https://api.idk.i-sp.in/get/presence')
@@ -12,14 +23,14 @@ export default function DiscordStatus() {
   if (!presence) return (<p>Loading...</p>)
 
   return (
-    <p className="dark:text-white text-center">
-      Current discord status: {presence.status}<br />
-      {!presence.custom ? "" :
+    <p className="text-center">
+      Discord status: {presence.status} <br />
+      {presence.activities?.map((p) => (
         <div className='flex justify-center'>
-          {presence.custom.emoji ? <img src={presence.custom.emoji} className="w-5 h-5 mr-4" alt="emoji" /> : ''}
-          {presence.custom.name} {presence.custom.text ? `- ${presence.custom.text}` : ''}
+          {p.emoji ? <img src={p.emoji} className="w-5 h-5 mr-4" alt="emoji" /> : ''}
+          {p.name} {p.state ? `- ${p.state}` : ''}
         </div>
-      }
+      ))}
     </p>
   )
 }
