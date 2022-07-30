5b2ea7bafbaa9d33f2c9df37e960d3d9d1cdd694
i-spin <me@i-sp.in>
2022-07-30 12:48:25 -0700

    update backend routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,23 +4,24 @@ import cero from '0http';
 import Authorization from './routes/post/authorization.js';
 import Home from './routes/home.js';
 import Data from './routes/post/data.js';
-import Mood from './routes/mood.js';
-import Middleware from './routes/Middleware.js';
-import Quote from './routes/quote.js';
-import Beat from './routes/beat.js';
-import Presence from './routes/presence.js';
+import Mood from './routes/get/mood.js';
+import Middleware from './routes/middleware.js';
+import Quote from './routes/get/quote.js';
+import Beat from './routes/get/beat.js';
+import Presence from './routes/get/presence.js';
 import startDiscordBot from './util/discord.js';
 
 const { router, server } = cero();
 
 const port = process.env.PORT || 8080;
 
-router.use('/', Middleware);
 router.get('/', Home);
-router.get('/mood', Mood);
-router.get('/quote', Quote);
-router.get('/beat', Beat);
-router.get('/presence', Presence);
+
+router.use('/get', Middleware);
+router.get('/get/mood', Mood);
+router.get('/get/quote', Quote);
+router.get('/get/beat', Beat);
+router.get('/get/presence', Presence);
 
 router.use('/post', Authorization);
 router.post('/post/data', Data);
