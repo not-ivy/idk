77f9b938891fcad682f0e7dfd6d164f0e9479a9b
i-spin <me@i-sp.in>
2022-08-01 17:08:27 -0700

    get range quotes

--- backend/src/routes/get/quote.js
+++ backend/src/routes/get/quote.js
@@ -1,7 +1,16 @@
-import { getLastData } from '../../util/prisma.js';
+import { getLastData, getRangeData } from '../../util/prisma.js';
 import serialize from '../../util/serialize.js';
 
-export default function Quote(_req, res) {
+export default function Quote(req, res) {
+  if (req.params.range) {
+    const [upper, lower] = req.params.range.split('-');
+    getRangeData('quotes', parseInt(upper, 10), parseInt(lower, 10)).then((data) => {
+      res.end(serialize(data));
+    }).catch((e) => {
+      res.end(e.toString());
+    });
+    return;
+  }
   getLastData('quotes').then((data) => {
     res.end(serialize(data));
   }).catch((e) => {
