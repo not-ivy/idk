309735de9dbbd53bec30acdedf44882ed151cd20
ivy <ivy@not-ivy.dev>
2022-09-05 17:09:05 -0700

    replace discord with spotify

---
+++ frontend/src/components/SpotifyStatus.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'preact/hooks';
+import type { Spotify } from '../types/spotify';
+import { backendUrl } from '../config.json';
+
+export default function SpotifyStatus() {
+  const [spotify, setSpotify] = useState<Spotify | Error | undefined>(undefined);
+
+  useEffect(() => {
+    fetch(`${backendUrl}/get/presence`)
+      .then((res) => res.json())
+      .then((data) => { setSpotify(data); console.log(data) })
+      .catch((error) => { setSpotify(error); console.log(error) });
+  }, [])
+
+  if (!spotify) return (<p>No spotify data.</p>)
+  if (spotify instanceof Error) return (<div>Error: <pre>{spotify.toString()}</pre></div>)
+
+  return (
+    <div>
+      Playing <a href={spotify.item.external_urls.spotify}>{spotify.item.name}</a> <br />
+      by {spotify.item.artists.map((a) => (<a href={a.external_urls.spotify}>{a.name}</a>)).join(', ')} <br />
+      in album <a className='underline' href={spotify.item.album.external_urls.spotify}>{spotify.item.album.name}</a> {spotify.repeat_state ? 'on repeat' : ''}
+    </div>
+  )
+}
