d771832e2f68983bac99b0d9732942d738aa83f8
ivy <ivy@not-ivy.dev>
2022-09-05 17:58:40 -0700

    try to fix artist

--- frontend/src/components/SpotifyStatus.tsx
+++ frontend/src/components/SpotifyStatus.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'preact/hooks';
+import { JSX } from 'preact';
 import type { Spotify } from '../types/spotify';
 import { backendUrl } from '../config.json';
 
@@ -15,10 +16,15 @@ export default function SpotifyStatus() {
   if (!spotify) return (<p>Loading...</p>)
   if (spotify instanceof Error) return (<div>Error: <pre>{spotify.toString()}</pre></div>)
 
+  const artists: JSX.Element[] = [];
+  spotify.item.artists.forEach((a) => {
+    artists.push(<a className='underline' href={a.external_urls.spotify}>{a.name}</a>)
+  })
+
   return (
     <div>
       {spotify.is_playing ? 'Playing' : 'Last played'} <a className='underline' href={spotify.item.external_urls.spotify}>{spotify.item.name}</a> <br />
-      by {spotify.item.artists.map((a) => (<a className='underline' href={a.external_urls.spotify}>{a.name}</a>))} <br />
+      by {artists.join(', ')} <br />
       in album <a className='underline' href={spotify.item.album.external_urls.spotify}>{spotify.item.album.name}</a> {spotify.repeat_state === 'off' ? '' : 'on repeat'}
     </div>
   )
